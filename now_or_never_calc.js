239a9e9415b3da7bf10e831e604c74a2d5e5cc89
Wes Yarber <austiny@mac.com>
2023-07-28 01:31:32 -0500

    fixed a bug where not all combinations were tried

--- now_or_never_calc.js
+++ now_or_never_calc.js
@@ -1,151 +1,133 @@
-// Web page and javascrupt code adapted from the python code mostly with ChatGPT
-
-function recursiveCompare(resourcesAvailable, sellingMethods, tradeActions = []) {
-  if (tradeActions.length > 0) {
-    tradeActions = [...tradeActions]; // Create a copy of the trade actions array
+class TradeOptimizer {
+  constructor(startingResourcesAvailable, tradeMethods) {
+      this.resourcesAvailable = startingResourcesAvailable;
+      this.tradeMethods = tradeMethods;
+      this.maxMoney = 0;
+      this.bestTrades = "";
+      this.remainingSingles = {};
   }
 
-  for (const [methodName, method] of Object.entries(sellingMethods)) {
-    if (Object.entries(resourcesAvailable).every(([key, value]) => key === 'money' || value >= -method[key])) {
-      const updatedResources = { ...resourcesAvailable };
-      for (const [key, value] of Object.entries(method)) {
-        updatedResources[key] += value;
+  checkTotalValue(resources, tradeActions) {
+      let totalValue = resources['money'] + Object.entries(resources)
+          .filter(([key]) => key !== 'money')
+          .reduce((acc, [key, value]) => acc + (['crystal', 'demon bottle', 'tool'].includes(key) ? value * 2 : value), 0);
+
+      if (totalValue > this.maxMoney) {
+          this.maxMoney = totalValue;
+          this.bestTrades = tradeActions.join('\n');
+          this.remainingSingles = Object.fromEntries(Object.entries(resources).filter(([key, value]) => value > 0 && key !== 'money'));
       }
+  }
 
-      tradeActions.push(methodName);
+  recursiveCompare(resourcesAvailable, tradeActions = []) {
+      for (let [method_name, method] of Object.entries(this.tradeMethods)) {
+          if (Object.entries(resourcesAvailable).every(([key, value]) => key === 'money' || value >= -method[key])) {
+              let updatedResources = { ...resourcesAvailable };
+              for (let [key, value] of Object.entries(method)) {
+                  updatedResources[key] += value;
+              }
 
-      if (updatedResources['money'] > maxMoney) {
-        maxMoney = updatedResources['money'];
-        bestTrades = tradeActions.join('\n');
+              tradeActions.push(method_name);
+
+              this.checkTotalValue(updatedResources, tradeActions);
+
+              this.recursiveCompare(updatedResources, tradeActions);
+              tradeActions.pop();
+          } else {
+              this.checkTotalValue(resourcesAvailable, tradeActions);
+          }
       }
+  }
 
-      recursiveCompare(updatedResources, sellingMethods, tradeActions);
-
-      tradeActions.pop();
-    } else {
-      const tempRemaining = { ...resourcesAvailable };
-      for (const [key, value] of Object.entries(resourcesAvailable)) {
-        if (key === 'money') {
-          continue;
-        }
-        if (key === 'shell') {
-          resourcesAvailable['money'] += value * 1;
-        } else if (['crystal', 'demon bottle', 'tool'].includes(key)) {
-          resourcesAvailable['money'] += value * 2;
-        } else {
-          console.error("Invalid resource key.");
-          return;
-        }
-        resourcesAvailable[key] = 0;
+  calculateMaxMoney() {
+      this.recursiveCompare(this.resourcesAvailable);
+      let resultText = `<p>Maximum money yield: ${this.maxMoney}</p>`;
+      resultText += '<p>Trades:</p>';
+      let trades = this.bestTrades.split('\n');
+      for (let i = 0; i < trades.length; i++) {
+          resultText += `<p>${trades[i]}</p>`;
       }
-
-      if (resourcesAvailable['money'] > maxMoney) {
-        maxMoney = resourcesAvailable['money'];
-        bestTrades = tradeActions.join('\n');
-        remainingSingles = tempRemaining;
+      // resultText += '<p>Sell Remaining Singles:</p>';
+      for (let [key, value] of Object.entries(this.remainingSingles)) {
+          if (value > 0 && key !== 'money') {
+              let additionalAmount = key === 'shell' ? value : 2 * value;
+              resultText += `<p>${value} individual ${key}${value > 1 ? 's' : ''}                                +${additionalAmount}</p>`;
+          }
       }
-    }
+      document.getElementById("result").innerHTML = resultText;
   }
 }
 
-// Initialize global variables
-let maxMoney = 0;
-let bestTrades = '';
-let remainingSingles = {};
-
-// Add selling methods
-const sellingMethods = {
-        '3 shell                                               +5': {
-            'crystal': 0,
-            'demon bottle': 0,
-            'tool': 0,
-            'shell': -3,
-            'money': 5
-        },
-        '2 tools + 1 shell                                +7': {
-            'crystal': 0,
-            'demon bottle': 0,
-            'tool': -2,
-            'shell': -1,
-            'money': 7
-        },
-        '2 demon bottles + 1 tool                   +9': {
-            'crystal': 0,
-            'demon bottle': -2,
-            'tool': -1,
-            'shell': 0,
-            'money': 9
-        },
-        '3 crystals                                          +11': {
-            'crystal': -3,
-            'demon bottle': 0,
-            'tool': 0,
-            'shell': 0,
-            'money': 11
-        },
-        '1 demon bottle + 1 tool + 1 shell      +9': {
-            'crystal': 0,
-            'demon bottle': -1,
-            'tool': -1,
-            'shell': -1,
-            'money': 9
-        },
-        '1 of each                              +12': {
-            'crystal': -1,
-            'demon bottle': -1,
-            'tool': -1,
-            'shell': -1,
-            'money': 12
-        },
-        '2 crystals + 2 demon bottles            +14': {
-            'crystal': -2,
-            'demon bottle': -2,
-            'tool': 0,
-            'shell': 0,
-            'money': 14
-        },
+let tradeMethods = {
+  '3 shell                                               +5': {
+      'crystal': 0,
+      'demon bottle': 0,
+      'tool': 0,
+      'shell': -3,
+      'money': 5
+  },
+  '2 tools + 1 shell                                +7': {
+      'crystal': 0,
+      'demon bottle': 0,
+      'tool': -2,
+      'shell': -1,
+      'money': 7
+  },
+  '2 demon bottles + 1 tool                   +9': {
+      'crystal': 0,
+      'demon bottle': -2,
+      'tool': -1,
+      'shell': 0,
+      'money': 9
+  },
+  '3 crystals                                          +11': {
+      'crystal': -3,
+      'demon bottle': 0,
+      'tool': 0,
+      'shell': 0,
+      'money': 11
+  },
+  '1 demon bottle + 1 tool + 1 shell      +9': {
+      'crystal': 0,
+      'demon bottle': -1,
+      'tool': -1,
+      'shell': -1,
+      'money': 9
+  },
+  '1 of each                              +12': {
+      'crystal': -1,
+      'demon bottle': -1,
+      'tool': -1,
+      'shell': -1,
+      'money': 12
+  },
+  '2 crystals + 2 demon bottles            +14': {
+      'crystal': -2,
+      'demon bottle': -2,
+      'tool': 0,
+      'shell': 0,
+      'money': 14
+  },
 };
 
-// Function to handle form submission
-function calculateMaxMoney(event) {
+document.getElementById("resourceForm").addEventListener("submit", function (event) {
   event.preventDefault();
-  maxMoney = 0;
-  bestTrades = '';
-  remainingSingles = {};
 
-  const startingResourcesAvailable = {
-    'crystal': parseInt(document.getElementById('crystal').value, 10),
-    'demon bottle': parseInt(document.getElementById('demonBottle').value, 10),
-    'tool': parseInt(document.getElementById('tool').value, 10),
-    'shell': parseInt(document.getElementById('shell').value, 10),
-    'money': 0
+  // Get the values from the form fields
+  var crystal = parseInt(document.getElementById("crystal").value);
+  var demonBottle = parseInt(document.getElementById("demonBottle").value);
+  var tool = parseInt(document.getElementById("tool").value);
+  var shell = parseInt(document.getElementById("shell").value);
+
+  // Build the resourcesAvailable object
+  var resourcesAvailable = {
+      'crystal': crystal,
+      'demon bottle': demonBottle,
+      'tool': tool,
+      'shell': shell,
+      'money': 0
   };
 
-  recursiveCompare(startingResourcesAvailable, sellingMethods);
-
-  // Display the results on the page
-  const resultDiv = document.getElementById('result');
-let resultText = `
-  <h2>Results:</h2>
-  <p>Maximum money yield: ${maxMoney}</p>
-  <p>Trades:<br>${bestTrades}
-`;
-
-for (const [key, value] of Object.entries(remainingSingles)) {
-  if (value > 0 && key !== 'money') {
-    let additional_amount = 0;
-    if (key == 'shell') {
-        additional_amount += value
-    }
-    else {
-        additional_amount += 2*value
-    }
-    resultText += `${value} individual ${key}${value > 1 ? 's' : ''}                                 +${additional_amount}`;
-  }
-}
-resultText += '</p>'
-
-resultDiv.innerHTML = resultText;
-}
-
-document.getElementById('resourceForm').addEventListener('submit', calculateMaxMoney);
+  var optimizer = new TradeOptimizer(resourcesAvailable, tradeMethods);
+  optimizer.calculateMaxMoney();
+});
--- now_or_never_calc.py
+++ now_or_never_calc.py
@@ -1,9 +1,56 @@
 import sys
 
-global max_money
-global trade_actions
-global remaining_singles 
-max_money = 0
+class TradeOptimizer:
+    def __init__(self, starting_resources_available, trade_methods):
+        self.resources_available = starting_resources_available
+        self.trade_methods = trade_methods
+        self.max_money = 0
+        self.best_trades = ""
+        self.remaining_singles = {}
+
+    def check_total_value(self, resources, trade_actions):
+        total_value = resources['money'] + sum([value * 2 if key in ['crystal', 'demon bottle', 'tool'] else value for key, value in resources.items() if key != 'money'])
+        if total_value > self.max_money:
+            self.max_money = total_value
+            self.best_trades = "\n".join(trade_actions)
+            self.remaining_singles = {key: value for key, value in resources.items() if value > 0 and key != 'money'}
+
+    def recursive_compare(self, resources_available, trade_actions=None):
+        if trade_actions is None:
+            trade_actions = []
+        
+        for method_name, method in self.trade_methods.items():
+            if all(value >= -method[key] for key, value in resources_available.items() if key != 'money'):
+                updated_resources = resources_available.copy()
+                for key, value in method.items():
+                    updated_resources[key] += value
+                trade_actions.append(method_name)
+
+                self.check_total_value(updated_resources, trade_actions)
+                
+                self.recursive_compare(updated_resources, trade_actions)
+                trade_actions.pop()
+            else:
+                self.check_total_value(resources_available, trade_actions)
+
+    def calculate_max_money(self):
+        self.recursive_compare(self.resources_available)
+        for key, value in self.remaining_singles.items():
+            addition_temp = value * 2 if key in ['crystal', 'demon bottle', 'tool'] else value
+            if key == "crystal":
+                extra_space = '     '
+            elif key == "demon bottle":
+                extra_space = ''
+            elif key == "tool":
+                extra_space = '        '
+            elif key == "shell":
+                extra_space = '       '
+            else:
+                print("invalid key:", key)
+            self.best_trades += f'\n{value} individual {key}{ "s" if value > 1 else " " }{extra_space}           +{addition_temp}'
+        print("\nMaximum money yield:", self.max_money)
+        print(f"\nTrades:\n{self.best_trades}")
+        print(f"Total:                               ={self.max_money}")
 
 # Add additional trades obtained through quest cards below if applicable
 # Note: Ensure the resources you trade in are negative values and money received is a positive value
@@ -15,113 +62,57 @@ trade_methods = {
         #     'tool': -3,
         #     'shell': -4,
         #     'money': 5
-        # },
-        '3 shell': {
-            'crystal': 0,
-            'demon bottle': 0,
-            'tool': 0,
-            'shell': -3,
-            'money': 5
-        },
-        '2 tools + 1 shell': {
-            'crystal': 0,
-            'demon bottle': 0,
-            'tool': -2,
-            'shell': -1,
-            'money': 7
-        },
-        '2 demon bottles + 1 tool': {
-            'crystal': 0,
-            'demon bottle': -2,
-            'tool': -1,
-            'shell': 0,
-            'money': 9
-        },
-        '3 crystals': {
-            'crystal': -3,
-            'demon bottle': 0,
-            'tool': 0,
-            'shell': 0,
-            'money': 11
-        },
-        '1 demon bottle + 1 tool + 1 shell': {
-            'crystal': 0,
-            'demon bottle': -1,
-            'tool': -1,
-            'shell': -1,
-            'money': 9
-        },
-        '1 of each': {
-            'crystal': -1,
-            'demon bottle': -1,
-            'tool': -1,
-            'shell': -1,
-            'money': 12
-        },
-        '2 crystals + 2 demon bottles': {
-            'crystal': -2,
-            'demon bottle': -2,
-            'tool': 0,
-            'shell': 0,
-            'money': 14
-        },
-    }
-
-def recursive_compare(resources_available, trade_methods, trade_actions=None):
-    global max_money
-    global best_trades
-    global remaining_singles
-
-    if trade_actions is None:
-        trade_actions = []
-
-    for method_name, method in trade_methods.items():
-        if all(value >= -method[key] for key, value in resources_available.items() if key != 'money'):
-            # Create a copy of resources_available before making trade actions
-            updated_resources = resources_available.copy()
-            for key, value in method.items():
-                updated_resources[key] += value
-            
-            trade_actions.append(method_name)
-
-            if updated_resources['money'] > max_money:
-                max_money = updated_resources['money']
-                best_trades = "\n".join(trade_actions)
-                
-            # Recursive call with the updated resources and trade actions
-            recursive_compare(updated_resources, trade_methods, trade_actions)
-
-            # Undo the trade action after exploring a branch
-            trade_actions.pop()
-        else:
-            temp_remaining = resources_available.copy()
-            for key, value in resources_available.items():
-                if key == 'money':
-                    continue
-                if key == 'shell':
-                    resources_available['money'] += value * 1
-                elif key in ['crystal', 'demon bottle', 'tool']:
-                    resources_available['money'] += value * 2
-                else:
-                    print("invalid resource key.")
-                    exit()
-                resources_available[key] = 0
-            
-            if resources_available['money'] > max_money:
-                max_money = resources_available['money']
-                best_trades = "\n".join(trade_actions)
-                remaining_singles = temp_remaining
-                
-def calculate_max_money(starting_resources_available):
-    global best_trades
-    global max_money
-    recursive_compare(starting_resources_available, trade_methods)
-    for key, value in remaining_singles.items():
-        if (value > 0) & (key != 'money'):
-            best_trades += f'\n{value} individual {key}s'
-        
-    print("Maximum money yield:", max_money)
-    print(f"Trades:\n{best_trades}")
+        # }, 
+    '3 shell                              +5': {
+        'crystal': 0,
+        'demon bottle': 0,
+        'tool': 0,
+        'shell': -3,
+        'money': 5
+    },
+    '2 tools + 1 shell                    +7': {
+        'crystal': 0,
+        'demon bottle': 0,
+        'tool': -2,
+        'shell': -1,
+        'money': 7
+    },
+    '2 demon bottles + 1 tool             +9': {
+        'crystal': 0,
+        'demon bottle': -2,
+        'tool': -1,
+        'shell': 0,
+        'money': 9
+    },
+    '3 crystals                           +11': {
+        'crystal': -3,
+        'demon bottle': 0,
+        'tool': 0,
+        'shell': 0,
+        'money': 11
+    },
+    '1 demon bottle + 1 tool + 1 shell    +9': {
+        'crystal': 0,
+        'demon bottle': -1,
+        'tool': -1,
+        'shell': -1,
+        'money': 9
+    },
+    '1 of each                            +12': {
+        'crystal': -1,
+        'demon bottle': -1,
+        'tool': -1,
+        'shell': -1,
+        'money': 12
+    },
+    '2 crystals + 2 demon bottles         +14': {
+        'crystal': -2,
+        'demon bottle': -2,
+        'tool': 0,
+        'shell': 0,
+        'money': 14
+    },
+}
 
 def get_user_input():
     starting_resources_available = {}
@@ -145,4 +136,5 @@ if __name__ == "__main__":
     else:
         starting_resources_available = get_user_input()
     
-    calculate_max_money(starting_resources_available)
+    optimizer = TradeOptimizer(starting_resources_available, trade_methods)
+    optimizer.calculate_max_money()
